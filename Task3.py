f51f943c199a3ffd560fe4f454614ded45d80941
Pritul Kumar Sinha <56963359+sanusinha2016@users.noreply.github.com>
2024-07-19 21:04:07 +0530

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -5,14 +5,13 @@ import pytesseract
 import cv2
 import matplotlib.pyplot as plt
 
-# Initialize Firebase Admin SDK
+
 cred = credentials.Certificate("exato-96bee-firebase-adminsdk-dft36-5a8af47540.json")
 firebase_admin.initialize_app(cred)
 
-# Create a Firestore client
 db = firestore.client()
 
-# Function to extract text from a handwritten PDF
+
 def extract_text_from_handwritten_pdf(pdf_path, image_path):
     old_pdf = Pdf.open(pdf_path)
     page = old_pdf.pages[0]
@@ -20,7 +19,7 @@ def extract_text_from_handwritten_pdf(pdf_path, image_path):
 
     pdf_image = PdfImage(raw_image)
     pdf_image.extract_to(fileprefix=image_path)
-    # print(list(page.images.keys()))
+
     print("processing")
 
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  
@@ -39,23 +38,23 @@ def extract_text_from_handwritten_pdf(pdf_path, image_path):
 
     return text
 
-# Path to the handwritten PDF file
+
 pdf_path = "input/handimages.pdf"
 image_path = "test1"
 
-# Extract text from the handwritten PDF
+
 extracted_text = extract_text_from_handwritten_pdf(pdf_path, image_path)
 
-# Prepare the document to be inserted
+
 document = {
     "file_name": pdf_path,
     "text_content": extracted_text
 }
 
-# Remove file extension for document ID (optional)
+
 document_id = pdf_path.split('/')[-1].split('.')[0]
 
-# Insert the document into Firestore with the PDF path as the document ID
+
 db.collection("Task 3").document(document_id).set(document)
 
 print("Handwritten PDF text successfully stored in Firestore.")
